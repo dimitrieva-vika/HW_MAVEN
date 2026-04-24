41ca178ca8e5d630d53491402ade299ffb2708cc
виктория димитриева <erschova.tamarah@yandex.ru>
2026-04-24 13:55:59 +1000

    Complete homework: BonusService with 4 tests

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BonusServiceTest {
+
+    @Test
+    void shouldCalculateForRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
+
+        long actual = service.calculate(amount, registered);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1_000_000;
+        boolean registered = true;
+        long expected = 500;
+
+        long actual = service.calculate(amount, registered);
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateForNotRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000;
+        boolean registered = false;
+        long expected = 10;  // 1% от 1000 = 10
+
+        long actual = service.calculate(amount, registered);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForNotRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 100_000;
+        boolean registered = false;
+        long expected = 500;  // 1% от 100000 = 1000, но лимит 500
+
+        long actual = service.calculate(amount, registered);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
